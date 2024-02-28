c13438109dcc7933c0f53de410e7e82cc3e63ef1
Reza Mousavi <iamrezamousavi@gmail.com>
2024-02-28 08:44:14 +0330

    add c-lib

---
+++ c-lib/calc.h
@@ -0,0 +1,6 @@
+#ifndef CALC_H
+#define CALC_H
+
+double add(double a, double b);
+
+#endif
---
+++ c-lib/main.cpp
@@ -0,0 +1,10 @@
+#include "calc.h"
+
+#include <iostream>
+
+int main() {
+  double a = add(14, 16);
+  std::cout << "A is ";
+  std::cout << a << std::endl;
+  return 0;
+}
