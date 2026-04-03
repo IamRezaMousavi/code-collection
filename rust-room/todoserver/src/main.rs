97567e31f9c4635e7cc6e76606b4a866fa76f56a
Reza Mousavi <iamrezamousavi@gmail.com>
2026-04-03 12:20:32 +0330

    refactor(rust): use clap instead of structopt in todoserver

--- rust-room/todoserver/src/main.rs
+++ rust-room/todoserver/src/main.rs
@@ -1,27 +1,28 @@
 /**
  * @Author: @iamrezamousavi
  * @Date:   2023-03-24 02:17:10
- * @Last Modified by:   @iamrezamousavi
- * @Last Modified time: 2023-03-26 21:23:50
+ * @Last Modified by:   Reza Mousavi
+ * @Last Modified time: 2026-04-03 12:14:28
  */
-use structopt::StructOpt;
+
+use clap::Parser;
 use todoserver::server::TodoServer;
 
-/// A minimul RESTful api
-#[derive(StructOpt, Debug)]
-#[structopt(name = "todoserver")]
-struct Opt {
-    /// Set host
-    #[structopt(short, long, default_value = "127.0.0.1")]
-    host: String,
+/// A minimal RESTful api
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    /// Set host ip
+    #[arg(short, long, default_value = "127.0.0.1")]
+    ip: String,
 
     /// Set port
-    #[structopt(short, long, default_value = "8080")]
+    #[arg(short, long, default_value = "8080")]
     port: u16,
 }
 
 fn main() {
-    let opt = Opt::from_args();
-    let server = TodoServer::new(opt.host, opt.port);
+    let args = Args::parse();
+    let server = TodoServer::new(args.ip, args.port);
     server.run().unwrap();
 }
